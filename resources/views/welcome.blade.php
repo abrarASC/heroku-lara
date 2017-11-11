d5d9b420093946b437b15f32db5318662940596e
AbrarASC <abrar1.asc@gmail.com>
2017-11-11 20:59:37 +0530

    Home page name change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Abrar Dhalwala
                 </div>
 
                 <div class="links">
